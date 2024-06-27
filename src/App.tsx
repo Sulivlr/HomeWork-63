8f22e0eaea16796ad3fc525f6f02eef507384aff
Sultan Mukhtarov <suli419956@gmail.com>
2024-06-27 14:40:59 +0600

    Created a component posts, where we have a state with types Post. Post has typification of object where we have value of that Post (strings). Now i fetched posts with hook useCallBack with axios did a get response from posts.json. And request i did with useEffect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import Appbar from './componets/AppBar/Appbar';
+import {Route, Routes} from 'react-router-dom';
+import Posts from './Containers/Posts/Posts';
+import NewPost from './Containers/NewPost/NewPost';
+
 const App = () => {
   return (
     <>
-      <h1>Hello!</h1>
+      <header>
+        <Appbar />
+      </header>
+      <main className="container-fluid">
+        <Routes>
+          <Route path="/" element={<Posts/>} />
+          <Route path="/new-post" element={<NewPost/>} />
+          <Route path="*" element={<h1>This Page Doesn't exist</h1>} />
+        </Routes>
+      </main>
     </>
   );
 };
